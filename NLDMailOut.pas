aab2693513a0ccf4f4c364e12b35227e9cd3f1e7
dany <dany@72e85ab5-d49d-6249-ada7-36e3088ace03>
2018-07-30 11:02:23 +0000

    git-svn-id: https://svn.apada.nl/svn/NLDelphi-opensource/dany/nlddanyutils@192 72e85ab5-d49d-6249-ada7-36e3088ace03

--- NLDMailOut.pas
+++ NLDMailOut.pas
@@ -2,6 +2,8 @@ unit NLDMailOut;
 
 // Dany Rosseel
 
+// Version for Indy 9
+
 { History of this unit
 28-10-2003: * Initial version
 20-03-2005: * Converted the "SendMail" procedures into functions. They
@@ -10,6 +12,8 @@ unit NLDMailOut;
 19-06-2018: * Simplified the implementation, does not uses the 'SMTPSetup' unit any more,
               instead the unit 'SMTP_Register' has been added (common for this unit ans 'SMTPSetup').
             * The routine 'Recall_Default_MailOut_Params_and_Addresses' is added.
+28-07-2018: * The 'init' routine has been removed. The registry items are not there... anymore
+30-07-2018: * Finaly got 'Attachments' working, thanks to "http://www.indyproject.org/Sockets/Blogs/RLebeau/2005_08_17_A.EN.aspx"          
 }
 
 {$WARN SYMBOL_PLATFORM OFF}
@@ -27,26 +31,26 @@ procedure SetMailOutAddresses(From, Answer: string);
 
 procedure Recall_Default_MailOut_Params_and_Addresses;
 
-function SendMail(Subject, Towards: string; Body: TStrings;
+function SendMail(Subject, Towards: string; Body_: TStrings;
   CC: string = ''; BCC: string = ''; Attachments: TStrings = nil): Boolean;
 overload;
 
-function SendMail(Subject, Towards, Body: string;
+function SendMail(Subject, Towards, Body_: string;
   CC: string = ''; BCC: string = ''; Attachments: TStrings = nil): Boolean;
 overload;
 
-function SendMail(Subject: string; Towards: Tstrings; Body: TStrings;
+function SendMail(Subject: string; Towards: Tstrings; Body_: TStrings;
   CC: string = ''; BCC: string = ''; Attachments: TStrings = nil): Boolean;
 overload;
 
-function SendMail(Subject: string; Towards: Tstrings; Body: string;
+function SendMail(Subject: string; Towards: Tstrings; Body_: string;
   CC: string = ''; BCC: string = ''; Attachments: TStrings = nil): Boolean;
 overload;
 
 implementation
 
 uses
-  SysUtils, IdMessage, NLDRcsStrings, NLDSimpleRegistry, SMTP_Register;
+  SysUtils, IdMessage, NLDRcsStrings, NLDSimpleRegistry, NLDSMTP_Register;
 
 var
   Info: record
@@ -92,14 +96,29 @@ begin
   Info.ReplyAddress := Answer;
 end;
 
+const
+  ContentTypes: array[0..8] of array[0..1] of string =
+  (
+    ('.txt', 'text/plain'),
+    ('.html', 'text/html'),
+    ('.htm', 'text/html'),
+    ('.jpeg', 'image/jpeg'),
+    ('.jpg', 'image/jpeg'),
+    ('.zip', 'application/x-zip-compressed'),
+    ('.gif', 'image/gif'),
+    ('.png', 'image/png'),
+    ('.rar', 'application/x-rar-compressed')
+    );
+
 function SendMail(Subject, Towards: string;
-                   Body: TStrings;
+                   Body_: TStrings;
                    CC: string = ''; BCC: string = '';
                    Attachments: TStrings = nil): Boolean;
 var
   Mess: TIdMessage;
   IdSMTP1: TIdSMTP;
-  I: Integer;
+  I,J: Integer;
+  Ext_: string;
 begin
   Result := false;
 
@@ -117,7 +136,7 @@ begin
     try
       Mess.Encoding := meMIME;
       Mess.AttachmentEncoding := 'MIME';
-      Mess.ContentType := 'text/plain';
+      Mess.ContentType := 'multipart/alternative';
       Mess.Charset := 'iso-8859-1';
       Mess.ContentTransferEncoding := '7bit';
 
@@ -127,19 +146,38 @@ begin
       Mess.CCList.EMailAddresses := CC; // comma separated addresses
       Mess.BCCList.EMailAddresses := BCC; // comma separated addresses
 
-      //Mess.from.Name := Info.EmailAddress;
       Mess.from.Address := Info.EmailAddress;
       Mess.ReplyTo.Emailaddresses := Info.ReplyAddress; // comma separated addresses
 
-      Mess.Body.Assign(Body);
+      Mess.Body.Assign(Body_);
 
       if Assigned(Attachments) then
       begin
         for I := 0 to Attachments.Count - 1 do
         begin
           if FileExists(Attachments[I]) then
-            TIdAttachment.Create(Mess.MessageParts, Attachments[I]);
+          begin
+            with TIdAttachment.Create(Mess.MessageParts, Attachments[I]) do
+            begin
+              ContentType := 'application/octet-stream'; // default (binary) contenttype
+
+              // find out the actual contenttype
+              Ext_ := ExtractFileExt(Attachments[I]);
+              for J := 0 to High(ContentTypes) do
+              begin
+                if Uppercase(Ext_) = Uppercase(ContentTypes[J, 0]) then
+                begin
+                  ContentType := ContentTypes[J, 1];
+                  break;
+                end;
+              end;
+
+              FileName := ExtractFileName(Attachments[I]);
+            end;
+            
+          end;
         end;
+        Mess.ContentType := 'multipart/mixed';
       end;
 
       try
@@ -163,21 +201,21 @@ begin
   end;
 end;
 
-function SendMail(Subject, Towards, Body: string;
+function SendMail(Subject, Towards, Body_: string;
   CC: string = ''; BCC: string = ''; Attachments: TStrings = nil): Boolean;
 var
   Bdy: TStrings;
 begin
   Bdy := TStringList.Create;
   try
-    Bdy.Add(Body);
+    Bdy.Add(Body_);
     Result := Sendmail(Subject, Towards, Bdy, CC, BCC, Attachments);
   finally
     Bdy.Free;
   end;
 end;
 
-function SendMail(Subject: string; Towards: Tstrings; Body: TStrings;
+function SendMail(Subject: string; Towards: Tstrings; Body_: TStrings;
   CC: string = ''; BCC: string = ''; Attachments: TStrings = nil): Boolean;
 var
   I: Integer;
@@ -189,10 +227,10 @@ begin
     if Towards[I] = '' then Towards.Delete(I);
   end;
   // send the mail
-  Result := SendMail(Subject, TStringsToString(Towards), Body, CC, BCC, Attachments);
+  Result := SendMail(Subject, TStringsToString(Towards), Body_, CC, BCC, Attachments);
 end;
 
-function SendMail(Subject: string; Towards: Tstrings; Body: string;
+function SendMail(Subject: string; Towards: Tstrings; Body_: string;
   CC: string = ''; BCC: string = ''; Attachments: TStrings = nil): Boolean;
 var
   I: Integer;
@@ -204,7 +242,7 @@ begin
     if Towards[I] = '' then Towards.Delete(I);
   end;
   // send the mail
-  Result := SendMail(Subject, TStringsToString(Towards), Body, CC, BCC, Attachments);
+  Result := SendMail(Subject, TStringsToString(Towards), Body_, CC, BCC, Attachments);
 end;
 
 procedure Recall_Default_MailOut_Params_and_Addresses;
